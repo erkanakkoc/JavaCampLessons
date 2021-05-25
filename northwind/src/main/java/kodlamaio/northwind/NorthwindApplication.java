ace585c6c37fecde1ba9c45aa5c192591c6c392f
akkoc.erkan@gmail.com <akkoc.erkan@gmail.com>
2021-05-25 17:50:59 +0300

    Update v2.1

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -3,8 +3,7 @@ package kodlamaio.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.PathSelectors;
+ 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -22,8 +21,7 @@ public class NorthwindApplication {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))                         
           .build();                                           
     }
 
